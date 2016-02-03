3cc1b52da2ac1d295c818c928281195a074d6981
Tom Durrant <t.durrant@metocean.co.nz>
2016-02-04 10:18:27 +1300

    Added some more filters - work in progress

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -16,16 +16,17 @@ lap2d=False
 
 class TestFilters(unittest.TestCase):
 
-    def setUp(self, ):
-        projection=ccrs.PlateCarree()
+    def setUp(self,):
+        self.projection=ccrs.PlateCarree()
         fname='./cfsr.nc'
         self.dset = xr.open_dataset(fname)
         fig, self.axs = plt.subplots(2, 2, figsize=(15,10),
-                                subplot_kw={'projection':projection})
+                                subplot_kw={'projection':self.projection})
         self.timestep = 2
         mpl=plotMap(self.dset.mslp[self.timestep],ax=self.axs[0,0],robust=True); plt.title("MSLP")
         self.vmin,self.vmax = mpl.get_clim()
-        cLat, cLon, rmax = -16.4, 149.8, 10
+        #cLat, cLon, rmax = -16.4, 149.8, 10
+        cLat, cLon, rmax = -16.4, 149.8, 30
         mu = 0
         sig = 5 * rmax
         delta = 3 * rmax
@@ -39,82 +40,98 @@ class TestFilters(unittest.TestCase):
         self.tmp2d[:] = 1
         self.tmp3d[:] = 1
         self.gauss = gauss(self.R,mu,sig) * self.tmp2d
+        self.bg={'mslp': 100000, 'ugrd10m': 0, 'vgrd10m': 0}
 
-    # def test_lap3d(self):
-    #     self.dset['lap'] = laplace(self.dset.mslp) * self.tmp3d
-    #     plotMap(self.dset.lap[self.timestep],ax=self.axs[1,0]); plt.title("Laplacian of MSLP")
+    def test_lap3d(self):
+        from scipy.ndimage.filters import laplace
+        self.dset['lap'] = laplace(self.dset.mslp) * self.tmp3d
+        plotMap(self.dset.lap[self.timestep],ax=self.axs[1,0]); plt.title("Laplacian of MSLP")
 
-    # def test_lap2d(self):
-    #     from scipy.ndimage.filters import laplace
-    #     from scipy.ndimage.filters import gaussian_laplace
-    #     from scipy.ndimage.filters import gaussian_filter
-    #     from scipy.ndimage.filters import gaussian_gradient_magnitude
-    #     # lap = gaussian_laplace(self.dset.mslp[self.timestep].values,2) * self.tmp2d
-    #     # plotMap(lap,ax=self.axs[1,0],robust=True); plt.title("Laplacian of MSLP")
-    #     # lap = gaussian_laplace(self.dset.mslp[self.timestep].values,10) * self.tmp2d
-    #     # plotMap(lap,ax=self.axs[1,1],robust=True); plt.title("Laplacian of MSLP")
-    #     image = self.dset.mslp[self.timestep].values
-    #     sig=10
-    #     plt.figure()
-    #     plt.imshow(gaussian_filter(image,sig)); plt.title('guassian')
-    #     plt.colorbar()
-    #     plt.figure()
-    #     plt.imshow(gaussian_laplace(image,sig)); plt.title('gl')
-    #     plt.colorbar()
-    #     plt.figure()
-    #     plt.imshow(gaussian_gradient_magnitude(image,sig)); plt.title('glm')
-    #     plt.colorbar()
+    def test_lap2d(self):
+        from scipy.ndimage.filters import laplace
+        from scipy.ndimage.filters import gaussian_laplace
+        from scipy.ndimage.filters import gaussian_filter
+        from scipy.ndimage.filters import gaussian_gradient_magnitude
+        image = self.dset.mslp[self.timestep].values
+        sig=10
+        plt.figure()
+        plt.imshow(gaussian_filter(image,sig)); plt.title('guassian')
+        plt.colorbar()
+        plt.figure()
+        plt.imshow(gaussian_laplace(image,sig)); plt.title('gl')
+        plt.colorbar()
+        plt.figure()
+        plt.imshow(gaussian_gradient_magnitude(image,sig)); plt.title('glm')
+        plt.colorbar()
 
     def test_gradient_removal(self):
         from scipy.ndimage.filters import gaussian_gradient_magnitude as ggm
         from scipy.ndimage.filters import gaussian_filter as gf
+        from scipy.signal import convolve2d
         image = self.dset.mslp[self.timestep].values
         grad = ggm(image,10)
-        dist = gf(self.R,10)**(1/2.)
-        gntmp = grad/grad.max() + dist.max()/dist
-        gn = gntmp/gntmp.max()
-        plotMap(gn*self.tmp2d,ax=self.axs[1,0],robust=True); plt.title("Gradiant Magnitude of MSLP")
-        out = ((1 - gn) * image) + (gn * image.mean())
-        plotMap(out*self.tmp2d,ax=self.axs[1,1],vmin=self.vmin, vmax=self.vmax); plt.title("Gradiant Magnitude of MSLP")
+        dist = gf(self.R,10)
+        dist =  self.R
+        #gntmp = grad/grad.max() * dist.max()/dist
+        #gn = grad/grad.max()
+        conv = convolve2d(grad/grad.max(),self.gauss/self.gauss.max(), mode='same')
+        gn = conv/conv.max()
+        plotMap(grad*self.tmp2d,ax=self.axs[1,0],robust=True); plt.title("Gradiant Magnitude of MSLP")
+        #out = ((1 - gn) * image) + (gn * self.bg['mslp'])
+        plotMap(conv*self.tmp2d,ax=self.axs[1,1]); plt.title("Convolution")
+        fig, self.axs = plt.subplots(3, 3, figsize=(18,15),
+                                subplot_kw={'projection':self.projection})
+        self.plotPair('mslp',gn,0,cmap=plt.get_cmap('viridis'))
+        self.plotPair('ugrd10m',gn,1,cmap=plt.get_cmap('RdBu'))
+        self.plotPair('vgrd10m',gn,2,cmap=plt.get_cmap('RdBu'))
+        plt.show()
 
-    # def test_2dgradient(self):
-    #     gradx, grady = np.gradient(self.dset.mslp[self.timestep])
-    #     mgrad = np.sqrt(gradx ** 2 + grady ** 2) 
-    #     ngrad = mgrad / mgrad.max() * self.tmp2d
-    #     plotMap(ngrad,ax=self.axs[1,0]); plt.title("Gradient")
-    #     plotMap(np.maximum(ngrad,self.gauss),ax=self.axs[1,1]); plt.title("Max(gradient,gaussian)")
+    def plotPair(self,var,gn,nn,**kwargs):
+        image = self.dset[var][self.timestep].values
+        mpl=plotMap(image*self.tmp2d,ax=self.axs[nn,0],robust=True,**kwargs); plt.title(var)
+        vmin,vmax = mpl.get_clim()
+        out = ((1 - gn) * image) + (gn * self.bg[var])
+        mpl=plotMap(out*self.tmp2d,ax=self.axs[nn,1],vmin=vmin,vmax=vmax,**kwargs); plt.title(var+' filtered')
+        mpl=plotMap(image - out*self.tmp2d,ax=self.axs[nn,2],robust=True,**kwargs); plt.title(var+' Difference')
 
-    # def test_skimage_dilate(self):
-    #     from skimage import data, io, filters
-    #     from skimage import img_as_float
-    #     from skimage.morphology import reconstruction
-    #     edges = filters.sobel(self.dset.mslp[self.timestep].values)
-    #     plotMap(edges*self.tmp2d,ax=self.axs[1,0]); plt.title("Skimage")
-    #     image = self.dset.mslp[self.timestep].copy()
-    #     seed = np.copy(image)
-    #     seed[1:-1, 1:-1] = image.min()
-    #     mask = image
-    #     dilated = reconstruction(seed, mask, method='dilation')
-    #     plotMap(image * self.tmp2d,ax=self.axs[1,1])
+    def test_2dgradient(self):
+        gradx, grady = np.gradient(self.dset.mslp[self.timestep])
+        mgrad = np.sqrt(gradx ** 2 + grady ** 2) 
+        ngrad = mgrad / mgrad.max() * self.tmp2d
+        plotMap(ngrad,ax=self.axs[1,0]); plt.title("Gradient")
+        plotMap(np.maximum(ngrad,self.gauss),ax=self.axs[1,1]); plt.title("Max(gradient,gaussian)")
 
-    # def test_watershed(self):
-    #     from skimage.morphology import watershed
-    #     from skimage.feature import peak_local_max
-    #     from scipy import ndimage as ndi
-    #     # Separate the two objects in image 
-    #     # Generate the markers as local maxima of the distance to the
-    #     # background
-    #     image = self.dset.mslp[self.timestep].values.copy()
-    #     #distance = ndi.distance_transform_edt(image)
-    #     local_maxi = peak_local_max(image, indices=False,
-    #                                 footprint=np.ones((3, 3)), labels=image)
-    #     markers = ndi.label(local_maxi)[0] 
-    #     labels = watershed(image, markers, mask=image)
-    #     #plotMap(-distance * self.tmp2d,ax=self.axs[1,0]);plt.title('Distance')
-    #     plotMap(-markers * self.tmp2d,ax=self.axs[1,1]);plt.title('Separated Peaks')
+    def test_skimage_dilate(self):
+        from skimage import data, io, filters
+        from skimage import img_as_float
+        from skimage.morphology import reconstruction
+        edges = filters.sobel(self.dset.mslp[self.timestep].values)
+        plotMap(edges*self.tmp2d,ax=self.axs[1,0]); plt.title("Skimage")
+        image = self.dset.mslp[self.timestep].copy()
+        seed = np.copy(image)
+        seed[1:-1, 1:-1] = image.min()
+        mask = image
+        dilated = reconstruction(seed, mask, method='dilation')
+        plotMap(image * self.tmp2d,ax=self.axs[1,1])
+
+    def test_watershed(self):
+        from skimage.morphology import watershed
+        from skimage.feature import peak_local_max
+        from scipy import ndimage as ndi
+        # Separate the two objects in image 
+        # Generate the markers as local maxima of the distance to the
+        # background
+        # Find peaks of background field within 100km of Pc
+        dscale = self.dset.mslp[self.timestep].copy()
+        image = dscale.values.copy()
+        local_maxi = peak_local_max(-image, indices=False,)
+        markers = ndi.label(local_maxi)[0] 
+        labels = watershed(image, markers, mask=image)
+        plotMap(self.R * self.tmp2d,ax=self.axs[1,0]);plt.title('Distance')
+        plotMap(markers * dscale,ax=self.axs[1,1]);plt.title('Separated Peaks')
+        plt.show()
 
 
 if __name__ == "__main__":
     unittest.main()
-    plt.show()
 
